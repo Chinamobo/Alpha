a47267dfd7ea54a64be7737083980617fffbcc76
BB9z <bb9z@me.com>
2014-04-11 17:53:54 +0800

    Design interface.

--- RFAPI/RFAPI.h
+++ RFAPI/RFAPI.h
@@ -1,10 +1,67 @@
 // TEST
 
-#import "AFHTTPRequestOperationManager.h"
+#import "RFAPIDefine.h"
+#import "RFInitializing.h"
+#import "AFNetworkReachabilityManager.h"
+#import "AFURLRequestSerialization.h"
 
 @class RFMessageManager;
+@protocol AFURLResponseSerialization;
 
-@interface RFAPI : AFHTTPRequestOperationManager
+@interface RFAPI : NSOperationQueue <
+    RFInitializing
+>
+
++ (instancetype)sharedInstance;
+
+@property (copy, nonatomic) NSURL *baseURL;
+
+@property (readonly, nonatomic) AFNetworkReachabilityManager *reachabilityManager;
+
+#pragma mark - Request
+
+@property (strong, nonatomic) AFHTTPRequestSerializer<AFURLRequestSerialization> *requestSerializer;
+
+/**
+ 
+ @param uploadResources
+
+ @code
+[
+    {
+        Name:@"value name",
+        Data:NSData
+    },
+    {
+        Name:@"value 2",
+        URL:NSURL for resource
+    },
+    {
+        Name:@"Optional keys",
+        Data:NSData,
+        FileName:@"file name",
+        MimeType:@"image/png",
+        Length:123
+    }
+]
+ @endcode
+
+ Support three source: `Data`, `URL`, `Stream`
+
+ @see AFMultipartFormData
+ */
+- (NSMutableURLRequest *)URLRequestWithDefine:(RFAPIDefine *)define parameters:(NSDictionary *)parameters uploadResources:(NSArray *)uploadResources;
+
+/**
+ For overwrite, default do nothing.
+ */
+- (NSMutableURLRequest *)customSerializedRequest:(NSMutableURLRequest *)request withDefine:(RFAPIDefine *)define;
+
+#pragma mark - Response
+
+@property (strong, nonatomic) id<AFURLResponseSerialization> responseSerializer;
+
+#pragma mark - Activity Indicator
 
 @property (strong, nonatomic) RFMessageManager *networkActivityIndicatorManager;
 
